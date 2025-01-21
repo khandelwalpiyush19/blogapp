ebe950abf13f2b97e3928ec18249fb7153a2dd24
Piyush Khandelwal <piyushkhandelwal482@gmail.com>
2025-01-21 20:10:33 +0530

    Update Cloudinary.js

--- Backend/libs/Cloudinary.js
+++ Backend/libs/Cloudinary.js
@@ -5,7 +5,6 @@ cloudinary.config({
     api_key: process.env.CLOUDINARY_API_KEY,
     api_secret: process.env.CLOUDINARY_API_SECRET
 });
-
 const FileUploadeToColoudinary = async (filepath, folderName) => {
     try {
         const result = await cloudinary.uploader.upload(filepath, {
